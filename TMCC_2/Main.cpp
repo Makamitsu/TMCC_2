e060032418b71967f23d31401c1f46a8b692c81a
Augustin Ambiehl <ambiehlaugustin@gmail.com>
2018-06-27 08:01:22 +0200

    [TMCC 2] first commit

---
+++ TMCC_2/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+* Given 2 numbers get PGCD 
+*/
+
+int main() {
+	
+	cout << "**************************\n";
+	cout << "******* EXERCICE 2 *******\n";
+	cout << "**************************\n";
+
+	unsigned long val1;
+	unsigned long val2;
+
+	cout << "PGCD Computation";
+	cout << "\nIndiquez valeur 1:";
+	cin >> val1;
+	cout << "\nIndiquez valeur 2:";
+	cin >> val2;
+
+	int result = 1;
+
+	cout << "\nPGCD:" << result;
+	system("PAUSE");
+
+}
